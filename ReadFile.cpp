f6fbd5743db5314df8fa84824b77d4664d5c1f70
ganyuvu <eeiliakas@gmail.com>
2024-11-27 20:44:38 +0000

    :sparkles: Implemented ReadFile
    -ReadFile can read the txt file I created, takes in a word, changes each char to lower case and places the word in a tree based on its key (the first letter)
    
    -Tested it out on main with a main menu implemented
    
    -Need implement a check for invalid input in main

---
+++ ReadFile.cpp
@@ -0,0 +1,53 @@
+#include "readFile.h"
+#include <fstream>
+
+using namespace std;
+
+//used code from previous bugs Life project to help write this section: https://github.com/ganyuvu/CA2_BugsLife_Ema_Eiliakas_D00255056/blob/master/Board.cpp
+void ReadWords(const std::string& fileName, TreeMap<char, BinaryTree<std::string>>& letterTreeMap) {
+
+    ifstream file(fileName); //opens file
+
+    //checks if file was opened
+    if (!file.is_open()) {
+
+        cout << "Unable to open file: " << fileName << endl;
+        return;
+    }
+
+    string word;
+
+    while (file >> word) {
+
+        //making sure word has characters, if not it will skip any blank space
+        if (!word.empty()) {
+
+            // converting the whole word to lowercase, to prevent case sensetivity 
+            for (size_t i = 0; i < word.size(); ++i) {
+                word[i] = tolower(word[i]); //goes through each char of the word and changed it to lowercase
+            }
+
+            char firstLetter = word[0];
+
+            //checking if the first letter already has a tree
+            if (!letterTreeMap.contains(firstLetter)) {
+                //if the letter doesnt have a tree we make a new binary tree for it
+                BinaryTree<string> newWordTree;
+
+                // puts the new tree into the map, the first letter is used as the key
+                // the new tree will store words starting with that letter
+                letterTreeMap.put(firstLetter, newWordTree);
+            }
+
+            // we find the reference of the tree based on the key (first letter of the word we want to add)
+            BinaryTree<string>& wordTree = letterTreeMap.get(firstLetter);
+
+            wordTree.add(word); // then we add it to the tree
+
+        }
+
+    }
+
+    file.close(); //closing the txt file
+
+}
--- main.cpp
+++ main.cpp
@@ -2,10 +2,76 @@
 #include <string>
 #include "TreeMap.h"
 #include "BinaryTree.h"
+#include "ReadFile.h"
 
 using namespace std;
 
+void displayMenu();
+
 int main() {
 
+    TreeMap<char, BinaryTree<string>> letterTreeMap;
+
+    string fileName = "testTextFile.txt"; //setting file path to a variable
+
+    //calls readWords() from readFile, gives it the filename and populates letterTreeMap
+    ReadWords(fileName, letterTreeMap);
+
+    int choice;
+
+    do {
+        displayMenu();
+        cin >> choice;
+
+        if (choice == 1) {
+
+            cout << "\nLetters in the TreeMap:" << endl;
+            //calls keySet method in treeMap and returns the keySet to the tree keys
+            BinaryTree<char> keys = letterTreeMap.keySet();
+
+            keys.printInOrder(); //calls printInOrder from BinaryTree 
+        }
+
+        else if (choice == 2) {
+            char letter;
+
+            cout << "\nEnter a letter: ";
+            cin >> letter;
+            letter = tolower(letter);
+
+            if (letterTreeMap.contains(letter)) {
+
+                //getting the tree reference with its values based on the key (letter)
+                BinaryTree<string>& words = letterTreeMap.get(letter);
+
+                cout << "\nWords starting with '" << letter << "': " << endl;
+                words.printInOrder(); //calls printInOrder from BinaryTree 
+            }
+           
+            else {
+                cout << "\nThere are no words for the letter '" << letter << "'" << endl;
+            }
+        }
+        
+        else if (choice == 3) {
+            cout << "\nGoodbyeeee" << endl;
+        }
+
+        //if user types anything but 1, 2 or 3 it will send this message
+        else {
+            cout << "\nError, please try again" << endl;
+        }
+    } 
+    
+    while (choice != 3);
+
     return 0;
+}
+
+void displayMenu() {
+    cout << "\n=====Menu=====" << endl;
+    cout << "1. Display letters" << endl;
+    cout << "2. Display words starting with a letter" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Enter choice of number: ";
 }
